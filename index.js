cad40794ab06169de3822cd1d21d93bcc553ae45
Rawshanara Eity <rawshanara.eity@gmail.com>
2023-08-31 23:05:23 +0600

    news category added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('logged')
+const handleCategory = async () =>{
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+    const data = await response.json();
+
+    const categoryContainer = document.getElementById('category-container')
+    const categories = data.data;
+    categories.forEach(category => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <button class="btn btn-active btn-ghost">${category.category}</button>
+        `;
+        categoryContainer.appendChild(div)
+    });
+    console.log(data.data)
+}
+handleCategory()
